aa5a59235ba199f9b3bce48515c208c94e729f25
Ryan Kwok <kwokryan11@gmail.com>
2025-12-29 21:14:15 -0800

    Created react frontend, sidebar ui completed.

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import Sidebar from "./custom/sidebar"
+
+function App() {
+
+  return (
+    <>
+    <Sidebar />
+    </>
+  )
+}
+
+export default App
---
+++ web/src/custom/sidebar.tsx
@@ -0,0 +1,19 @@
+import { Button } from "../components/ui/button";
+
+
+const Sidebar = () => {
+    return (
+        <div className="sticky top-0 z-50 flex flex-row items-center p-4 border-b">
+            <header className="text-xl cursor-pointer font-bold">Theta Tau</header>
+            <div className="flex ml-auto  space-x-4 items-center">
+                <p className="text-gray-500 cursor-pointer">Members</p>
+                <Button className="cursor-pointer">Sign in</Button>
+
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Sidebar;
