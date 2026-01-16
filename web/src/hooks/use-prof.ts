32f16554f7e3f9b444324511325ee87ae6f3ccf4
Ryan Kwok <kwokryan11@gmail.com>
2026-01-15 22:05:47 -0800

    Wrote hooks for profile.

---
+++ web/src/hooks/use-prof.ts
@@ -0,0 +1,42 @@
+import { getAllUsers, getUserById, getUserProfile } from "../data/api";
+import { toast } from "sonner";
+
+
+function useProfile() {
+    const fetchUsers = async () => {
+        try {
+            const users = await getAllUsers();
+            return users;
+        } catch (error) {
+            const errorMessage = (error as Error).message ?? "Couldn't fetch users! Please try again later!";
+            toast.error("Fetch users failed: " + errorMessage);
+            return false;
+        }
+    };
+
+    const fetchUserById = async (userId: number) => {
+        try {
+            const user = await getUserById(userId);
+            return user;
+        } catch (error) {
+            const errorMessage = (error as Error).message ?? "Couldn't fetch user! Please try again later!";
+            toast.error("Fetch user failed: " + errorMessage);
+            return false;
+        }
+    };
+
+    const fetchCurrentUserProfile = async () => {
+        try {
+            const profile = await getUserProfile();
+            return profile;
+        } catch (error) {
+            const errorMessage = (error as Error).message ?? "Couldn't fetch profile! Please try again later!";
+            toast.error("Fetch profile failed: " + errorMessage);
+            return false;
+        }
+    };
+    return { fetchUsers, fetchUserById, fetchCurrentUserProfile }
+
+};
+
+export default useProfile;
