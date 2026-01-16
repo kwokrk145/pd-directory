fc4df188be8f98eb62ad32cda2dfbb3efb005ae8
Ryan Kwok <kwokryan11@gmail.com>
2026-01-15 20:41:18 -0800

    Wrote routes for updating and deleting experiences.

--- api/src/routes/experience.ts
+++ api/src/routes/experience.ts
@@ -12,7 +12,6 @@ experienceRouter.post("/", async (req, res) => {
             message: "Unauthorized"
         });
     }
-
     try {
         
         const newExperience = await prisma.experience.create({
@@ -34,4 +33,81 @@ experienceRouter.post("/", async (req, res) => {
 
 });
 
+
+experienceRouter.patch("/:experienceId", async (req, res) => {
+    const userId = req.session.userId;
+    if (!userId) {
+        return res.status(401).json({
+            message: "Unauthorized"
+        });
+    }
+    const experienceId = Number(req.params.experienceId);
+    if (isNaN(experienceId)) {
+        return res.status(400).json({
+            message: "Invalid experience ID"
+        });
+    }
+
+    const experience = await prisma.experience.findUnique({
+        where  : { id: experienceId }
+    });
+    if (!experience || experience.userId !== userId) {
+        return res.status(404).json({
+            message: "Experience not found"
+        });
+    }
+    const { title, organization, startDate, endDate, description } = req.body;
+    try {
+        const updatedExperience = await prisma.experience.update({
+            where: { id: experienceId },
+            data: {
+                title,
+                organization,
+                startDate,
+                endDate,
+                description,
+            },
+        });
+        return res.status(200).json(updatedExperience);
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal server error"
+        });
+    }
+});
+
+experienceRouter.delete("/:experienceId", async (req, res) => {
+    const userId = req.session.userId;
+    if (!userId) {
+        return res.status(401).json({
+            message: "Unauthorized"
+        });
+    }
+    const experiencedId = Number(req.params.experienceId);
+    if (isNaN(experiencedId)) {
+        return res.status(400).json({
+            message: "Invalid experience ID"
+        });
+    }
+    const experience = await prisma.experience.findUnique({
+        where  : { id: experiencedId }
+    });
+    if (!experience || experience.userId !== userId) {
+        return res.status(404).json({
+            message: "Experience not found"
+        });
+    }
+    try {
+        await prisma.experience.delete({
+            where: { id: experiencedId }
+        });
+        return res.status(200).json({
+            message: "Experience deleted"
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal server error"
+        });
+    }
+});
 export default experienceRouter;
